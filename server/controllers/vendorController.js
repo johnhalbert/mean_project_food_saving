1b49d180f33ba5ea759ed0dc9b38d8649b0efea9
John Halbert <johnhalbert@gmail.com>
2015-07-06 20:47:58 -0700

    Updated vendorController.js

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -3,15 +3,50 @@ var Vendor = mongoose.model('Vendor');
 
 module.exports = {
 	createVendor: function(req, res){
-		var newVendor = new Vendor({name: req.body.name, type: req.body.type, address: req.body.address, phone: req.body.phone, hours: req.body.hours})
+		var newVendor = new Vendor({name: req.body.name, type: req.body.type, address: req.body.address, phone: req.body.phone, hours: req.body.hours});
+		newVendor.save(function(err, vendor){
+			if (err) {
+				console.log('Error creating new vendor', err);
+			} else {
+				res.json(vendor);
+			}
+		})
 	},
 	updateVendor: function(req, res){
-
+		Vendor.findOne({'_id': req.params.id}, function(err, vendor){
+			if (err) {
+				console.log('Error while updating vendor (1)', err);
+			} else {
+				vendor.name: req.body.name;
+				vendor.address: req.body.address;
+				vendor.phone: req.body.phone;
+				vendor.hours: req.body.hours;
+				vendor.save(function(err, vendor){
+					if (err) {
+						console.log('Error while updating vendor (2)', err);
+					} else {
+						res.json(vendor);
+					}
+				})
+			}
+		})
 	},
 	retrieveVendor: function(req, res){
-
+		Vendor.findOne({'_id': req.params.id}, function(err, vendor){
+			if (err) {
+				console.log('Error while retrieving vendor', err);
+			} else {
+				res.json(vendor);
+			}
+		})
 	},
 	retrieveVendors: function(req, res){
-
+		Vendor.find({}, function(err, vendors){
+			if (err) {
+				console.log('Error while retrieving vendor list', err);
+			} else {
+				res.json(vendors);
+			}
+		})
 	}
 }
