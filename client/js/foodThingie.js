94a9b858d15e5c9d011f6cf4a04990c3fb1c52c5
ar293in2015 <ar293@duke.edu>
2015-07-07 10:24:16 -0700

    fixed missing curly brace error

--- client/js/foodThingie.js
+++ client/js/foodThingie.js
@@ -16,6 +16,7 @@ foodThingie.config(function($routeProvider){
         })
         .when('/groceryList', {
         templateUrl: 'partials/grocery_list.html'
+        })
 		.otherwise({redirectTo:'/'});
 })
 
