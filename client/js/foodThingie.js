f25c8eddef871cabcf95efacd8aec2e86193f464
John Halbert <johnhalbert@gmail.com>
2015-07-06 21:40:19 -0700

    Updated factories for orders, products, vendors in foodthingie.js

--- client/js/foodThingie.js
+++ client/js/foodThingie.js
@@ -98,12 +98,79 @@ foodThingie.factory('socket', function ($rootScope) {
 //factory for vendors
 foodThingie.factory("vendorFactory", function($http){
     var factory = {};
+
+    factory.createVendor = function(vendor, callback){
+        $http.post('/vendors/new', vendor)
+          .success(function(vendor){
+            callback(vendor);
+          })
+    }
+
+    factory.updateVendor = function(vendor, callback){
+      $http.post('/vendors/'+vendor._id+'/update', vendor)
+        .success(function(vendor){
+          callback(vendor);
+        })
+    }
+
+    factory.retrieveVendor = function(vendor, callback){
+      $http.get('/vendors/'+vendor._id+'/show')
+        .success(function(vendor){
+          callback(vendor);
+        })
+    }
+
+    factory.retrieveVendors = function(callback){
+      $http.get('/vendors')
+        .success(function(vendors){
+          callback(vendors);
+        })
+    }
+
     return factory;
 })
 //factory for products
 foodThingie.factory("productFactory", function($http){
+    
     var factory = {};
+
+    factory.createProduct = function(product, callback){
+      $http.post('/products/new', product)
+        .success(function(product){
+          callback(product);
+        })
+    }
+
+    factory.updateProduct = function(product, callback){
+      $http.post('/products/'+product._id+'/update', product)
+        .success(function(product){
+          callback(product);
+        })
+    }
+
+    factory.retrieveProduct = function(product, callback){
+      $http.get('/products/'+product._id)
+        .success(function(product){
+          callback(product);
+        })
+    }
+
+    factory.retrieveProducts = function(callback){
+      $http.get('/products')
+        .success(function(products){
+          callback(products);
+        })
+    }
+
+    factory.destroyProduct = function(product, callback){
+      $http.post('/products/'+product._id+'/destroy')
+        .success(function(product){
+          callback(product);
+        })
+    }
+
     return factory;
+
 })
 //factory for customers
 foodThingie.factory("customerFactory", function($http){
@@ -112,12 +179,54 @@ foodThingie.factory("customerFactory", function($http){
 })
 //factory for orders
 foodThingie.factory("orderFactory", function($http){
+
     var factory = {};
+
+    factory.createOrder = function(order, callback){
+      $http.post('/orders/new', order)
+        .success(function(order){
+          callback(order);
+        })
+    }
+
+    factory.destroyOrder = function(order, callback){
+      $http.post('/orders/'+order._id+'/destroy', order){
+        .success(function(order){
+          callback(order);
+          // This will probably change.
+        })
+      }
+    }
+
+    factory.retrieveOrder = function(order, callback){
+      $http.get('/orders/'+order._id)
+        .success(function(order){
+          callback(order);
+        })
+    }
+
+    factory.retrieveOrders = function(callback){
+      $http.get('/orders')
+        .success(function(orders){
+          callback(orders);
+        })
+    }
+
+    factory.updateOrder = function(order, callback){
+      $http.post('/orders/'+order._id+'/update', order)
+        .success(function(order){
+          callback order;
+        })
+    }
+  
     return factory;
+
 })
 //controller for pie chart
 foodThingie.controller("piechart", function(piechartFactory){
+
 })
 //controller for dash
 foodThingie.controller('DashCtrl', function($scope, socket){
+
 })
