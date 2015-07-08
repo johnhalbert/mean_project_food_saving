bc52012576df9a7f0f3620cd59ee83a1dbbba7d5
ar293in2015 <ar293@duke.edu>
2015-07-08 14:29:11 -0700

    geencoder for integrating google maps api input address

---
+++ client/js/googleMapsGeoencoder.js
@@ -0,0 +1,38 @@
+//https://developers.google.com/maps/documentation/javascript/geocoding
+
+// COPY THIS INTO A TEST FILE
+
+var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  }
+
+  function codeAddress() {
+    var address = document.getElementById("address").value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+<body onload="initialize()">
+ <div id="map-canvas" style="width: 320px; height: 480px;"></div>
+  <div>
+    <input id="address" type="textbox" value="Sydney, NSW">
+    <input type="button" value="Encode" onclick="codeAddress()">
+  </div>
+</body>
