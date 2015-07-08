3e67b9a57a2854450cfb351b78cba1408684acc4
John Halbert <johnhalbert@gmail.com>
2015-07-07 20:29:43 -0700

    This is for jay

--- client/js/foodThingie.js
+++ client/js/foodThingie.js
@@ -439,7 +439,7 @@ foodThingie.controller('infoController', function($window, $scope, socket, $rout
 
 })
 //controller for individual store/restaurant
-foodThingie.controller('indiController', function($window, $scope, socket, $routeParams, customerFactory, vendorFactory){
+foodThingie.controller('indiController', function($window, $scope, socket, $routeParams, customerFactory, vendorFactory, productFactory){
 
     customerFactory.retrieveLogin(function(customer){
         if (customer) {
@@ -452,6 +452,16 @@ foodThingie.controller('indiController', function($window, $scope, socket, $rout
     vendorFactory.retrieveVendor($routeParams.id, function(vendor){
         $scope.vendor = vendor;
         console.log($scope.vendor);
+        productFactory.retrieveProductsOfVendor($scope.vendor, function(products){
+              console.log('getting products', $scope.vendor);
+            if (products.error) {
+                console.log(products.error);
+                $scope.error = products.error;
+            } else {
+                console.log('Success!', products);
+                $scope.products = products;
+            }
+        })
     })
 
 })
