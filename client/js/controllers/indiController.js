8cc8439908a54cf3681d13aca6077129444e7074
ar293in2015 <ar293@duke.edu>
2015-07-08 17:16:35 -0700

    adding orders methods in controller

--- client/js/controllers/indiController.js
+++ client/js/controllers/indiController.js
@@ -1,4 +1,5 @@
 foodThingie.controller('indiController', function($window, $scope, socket, $routeParams, customerFactory, vendorFactory, productFactory){
+    $scope.pendingOrder = [];
 
     customerFactory.retrieveLogin(function(customer){
         if (customer) {
@@ -20,4 +21,17 @@ foodThingie.controller('indiController', function($window, $scope, socket, $rout
         }
     })
 
+    $scope.addItem = function(product_id){
+        console.log("HERE", product_id);
+        $scope.orderProduct = {};
+        for (var i = 0; i < $scope.products.products.length; i++) {
+            if ($scope.products.products[i]._id === product_id) {
+                $scope.orderProduct.product = $scope.products.products[i];
+            }
+        }
+        $scope.pendingOrder.push({product: $scope.orderProduct, quantity: $scope.newOrder.quantity});
+        console.log($scope.pendingOrder);
+        $scope.newOrder.products.push($scope.orderProduct._id);
+        $scope.orderProduct = {};
+    }
 })
